d23a389ad6e823f2cd1c5ec6fa8cfffece622bdf
Amanpreet Singh <amsingh@amans-macapple.lan>
2020-03-11 09:17:43 -0700

    First commit for server

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+
+const data = [{
+    id: 1,
+    firstName: 'Aman',
+    lastName: 'Singh'
+},
+{
+    id: 2,
+    firstName: 'Avitesh',
+    lastName: 'Singh'
+}];
+
+app.get('/users', (req, res) => {
+    res.send(data);
+});
+
+
+const start = () => {
+    app.listen(3001, () => {
+        console.log('Server is up at 3001');
+    });
+};
+
+module.exports = start;
